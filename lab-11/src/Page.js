34f28397197354a1f447b68a35369dc5e48f8732
Sean Yue <71090882+syue40@users.noreply.github.com>
2022-11-24 11:09:56 -0800

    display no results when no pokemon found

--- lab-11/src/Page.js
+++ lab-11/src/Page.js
@@ -3,15 +3,19 @@ import Pokemon from './Pokemon'
 function page({ currentPokemons, currentPage }) {
   return (
     <div>
-      <h1>
-        Page number {currentPage}
+      <h1 class="flex justify-center mt-5 mb-4">
+        Page Number: {currentPage}
       </h1>
       <div className="pokemon-list">
-        {
-          currentPokemons.map(item => {
-            return <Pokemon key={item.id} pokemon={item} />
-          })
-        }
+      {currentPokemons.length > 0 ? 
+        <div className="pokemon-list">
+          {
+            currentPokemons.map(item => {
+              return <Pokemon key={item.id} pokemon={item} />
+            })
+          }
+        </div> : 
+        <div><h1 class="text-xl font-bold">No Results</h1></div>}
       </div>
     </div>
   )
