f31bdb3e71d0f4c6334dcff8191e737a2049c361
Sean Yue <71090882+syue40@users.noreply.github.com>
2022-11-24 13:16:56 -0800

    add search by name and id

--- lab-11/src/FilteredPagination.js
+++ lab-11/src/FilteredPagination.js
@@ -12,14 +12,30 @@ function FilteredPagination({ types, checkedState }) {
     axios.get('https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json')
       .then(res => res.data)
       .then(data => {
-        // filter based on the checkedState
-        console.log(checkedState.types_array);
-        data = (data.filter(pokemon => checkedState.types_array.every((checked, i) => !checked || pokemon.type.includes(types.current[i]))
-        // data = (data.filter(pokemon =>))
-        ))
-        if(checkedState.health_range) data = (data.filter(pokemon => pokemon.base.HP >= checkedState.health_range[0] && pokemon.base.HP <= checkedState.health_range[1]))
-        if(checkedState.attack_range) data = (data.filter(pokemon => pokemon.base.Attack >= checkedState.attack_range[0] && pokemon.base.Attack <= checkedState.attack_range[1]))
-        // console.log(data);
+        // filter based on the applied filters
+        if(checkedState.name){
+          data = (data.filter(pokemon => pokemon.name.english.toLowerCase() === checkedState.name.toLowerCase()))
+          return data
+        }
+
+        if(checkedState.id){
+          data = (data.filter(pokemon => pokemon.id.toString() === checkedState.id.toString()))
+          return data
+        }
+
+        if(checkedState.types_array) data = (data.filter(pokemon => 
+          checkedState.types_array.every(
+            (checked, i) => !checked || pokemon.type.includes(types.current[i])
+          )))
+          
+        if(checkedState.health_range) data = (data.filter(pokemon => 
+          pokemon.base.HP >= checkedState.health_range[0] && pokemon.base.HP <= checkedState.health_range[1]
+          ))
+
+        if(checkedState.attack_range) data = (data.filter(pokemon => 
+          pokemon.base.Attack >= checkedState.attack_range[0] && pokemon.base.Attack <= checkedState.attack_range[1]
+          ))
+        
         return data
       })
       .then(res => {
--- lab-11/src/Search.js
+++ lab-11/src/Search.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
-
+import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
+const ariaLabel = { 'aria-label': 'description' };
 function Search({ types, checkedState, setCheckedState }) {
   var currentTypes = checkedState.types_array;
-  
+
   const handleClearFilters = () => {
     window.location.reload(false);
+  }
+
+  const handleNameClick = () => {
+    setCheckedState({"name" : nameText, "types_array": currentTypes, "health_range": healthValue, "attack_range": attackValue})
+  }
+
+  const handleIdClick = () => {
+    setCheckedState({"id" : idText, "types_array": currentTypes, "health_range": healthValue, "attack_range": attackValue})
   }
 
   const handleButtonClick = () => {
@@ -14,16 +24,15 @@ function Search({ types, checkedState, setCheckedState }) {
   }
 
   const onChangeHandle = (type) => {
-    console.log(type)
     const index = types.current.indexOf(type);
     currentTypes[index] === true ? currentTypes[index] = false : currentTypes[index] = true
-
-    console.log(currentTypes)
   }
 
   // const [newCheckedState, setNewCheckedState] = React.useState([])
   const [healthValue, setHealthValue] = React.useState([0, 300]);
   const [attackValue, setAttackValue] = React.useState([0, 200]);
+  const [nameText, setNameText] = React.useState("")
+  const [idText, setIdText] = React.useState("")
 
   const handleHealthChange = (event, newValue) => {
     setHealthValue(newValue);
@@ -33,15 +42,54 @@ function Search({ types, checkedState, setCheckedState }) {
     setAttackValue(newValue);
   };
 
+  const handleNameTextChange = (event) => {
+    setNameText(event.target.value);
+  }
+
+  const handleIdTextChange = (event) => {
+    setIdText(event.target.value);
+  }
+
   return (
     <div>
       <h1 class="flex justify-center text-xl font-bold bg-black text-white p-3">Search Filters</h1>
       <div class="flex justify-center">
-        <div class="mr-5">
-          <h1 class="mt-3">Health Filter</h1>
+        <div class="mr-5 mt-3 pr-5">
+          <div class="mb-10">
+          <h1 class="font-bold">Search by Name: </h1>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1.5, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <Input placeholder="Pikachu" inputProps={ariaLabel} onChange={handleNameTextChange}/>
+          </Box>
+          <button class="rounded-lg bg-gray-300 hover:bg-gray-200 active:bg-gray-100 p-2 m-3" onClick={handleNameClick}>Search Name</button>
+          </div>
+          <div>
+          <h1 class="font-bold">Search by ID: </h1>
+          <Box
+            component="form"
+            sx={{
+              '& > :not(style)': { m: 1.5, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <Input placeholder="96" inputProps={ariaLabel} onChange={handleIdTextChange}/>
+            
+          </Box>
+          <button class="rounded-lg bg-gray-300 hover:bg-gray-200 active:bg-gray-100 p-2 m-3" onClick={handleIdClick}>Search ID</button>
+          </div>
+        </div>
+        <div class="mr-5 mt-3 ml-5">
+          <h1 class="font-bold">Health Range</h1>
           <div class="flex mt-4">
             <div class="mr-3">{healthValue[0]}</div>
-              <Box sx={{ width: 300 }}>
+              <Box sx={{ width: 250 }}>
                 <Slider
                   min={0}
                   max={300}
@@ -53,10 +101,10 @@ function Search({ types, checkedState, setCheckedState }) {
               </Box>
             <div class="ml-3">{healthValue[1]}</div>
           </div>
-          <h1 class="mt-3">Attack Filter</h1>
+          <h1 class="mt-5 font-bold">Attack Range</h1>
           <div class="flex mt-4">
             <div class="mr-3">{attackValue[0]}</div>
-              <Box sx={{ width: 300 }}>
+              <Box sx={{ width: 250 }}>
                 <Slider
                 min={0}
                 max={200}
@@ -69,7 +117,8 @@ function Search({ types, checkedState, setCheckedState }) {
             <div class="ml-3">{attackValue[1]}</div>
           </div>
         </div>
-        <div class="w-1/4 m-4">
+        <div class="w-1/4 m-3 ml-5">
+        <h1 class="font-bold mb-2">Types Filter</h1>
           <div class="grid grid-cols-3 gap-2">
             {
               types.current.map(type => {
@@ -86,10 +135,10 @@ function Search({ types, checkedState, setCheckedState }) {
         </div>
       </div>
       <div class="flex justify-center mt-4">
-          <button class="bg-black hover:bg-gray-600 active:bg-gray-500 text-white p-5 m-4" onClick={handleButtonClick}>
+          <button class="rounded-lg bg-black hover:bg-gray-600 active:bg-gray-500 text-white p-5 m-4" onClick={handleButtonClick}>
             Submit
           </button>
-          <button class="bg-black hover:bg-gray-600 active:bg-gray-500 text-white p-5 m-4" onClick={handleClearFilters}>
+          <button class="rounded-lg bg-black hover:bg-gray-600 active:bg-gray-500 text-white p-5 m-4" onClick={handleClearFilters}>
             Clear Filters
           </button>
         </div>
