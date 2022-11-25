cfcc7b3a4e5ea3bb4d309d3483be870deece67fa
Sean Yue <71090882+syue40@users.noreply.github.com>
2022-11-24 22:24:26 -0800

    add stylying to pokedex entry

--- lab-11/src/PokemonProfile.js
+++ lab-11/src/PokemonProfile.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import React, { useEffect, useState } from 'react'
 function PokemonProfile(pokemon) {
-    const [currentPokemon, setCurrentPokemon] = useState("")
+    const navigate = useNavigate();
     var matches = []
     const [pokemons, setPokemonList] = useState({
         "base": {
@@ -56,15 +57,18 @@ function PokemonProfile(pokemon) {
 
     return (
         <div>
-            <div >
-                <h1 class="flex bg-black text-white text-xl p-5 content-center justify-center">{pokemons.name.english}</h1>
+            <div class="flex bg-black text-white text-xl p-5 justify-center" >
+                <h1>{pokemons.name.english}</h1>
             </div>
-            <div>
+            <div class="flex justify-center mt-10">
+            <div class="border-solid border-2 border-gray-700 p-4">
             {
-                pokemon.pokemon ? <img src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${getThreeDigitId(pokemon.pokemon)}.png`} /> :
-                <img src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${getThreeDigitId(matches[0])}.png`} />
+                pokemon.pokemon ? <img class="m-4 h-72 w-72" src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${getThreeDigitId(pokemon.pokemon)}.png`} /> :
+                <img class="m-4 h-72 w-72" src={`https://github.com/fanzeyi/pokemon.json/raw/master/images/${getThreeDigitId(matches[0])}.png`} />
             }
-            
+            </div>
+            <div class="ml-5 border-solid border-2 border-black">
+            <h1 class="flex text-white bg-black content-center justify-center p-3">Stats</h1>
             <ul class="content-center m-5">
                 <li>HP: {pokemons.base.HP}</li>
                 <li>Attack: {pokemons.base.Attack}</li>
@@ -75,8 +79,9 @@ function PokemonProfile(pokemon) {
                 <li>Type(s): {pokemons.type.map(index => index+" ")}</li>
             </ul>
             </div>
-            <div class="">
-                
+            </div>
+            <div class="flex content-center justify-center">
+                <button class="rounded-lg bg-black hover:bg-gray-600 active:bg-gray-500 text-white p-5 m-4" onClick={() => navigate(-1)}>Back</button>
             </div>
         </div>
   )
